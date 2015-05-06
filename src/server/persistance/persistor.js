4243e8585a5d641f7611997b9d781eb02be3b207
Romeo Van Snick <romeovs@gmail.com>
2015-05-06 11:29:17 +0200

    add documentation and make lastRev 'public'

--- src/server/persistance/persistor.js
+++ src/server/persistance/persistor.js
@@ -1,3 +1,40 @@
+// This file contains the Persistor class
+// which all Persistors should extend.
+//
+// A Persistor is the driver behind
+// the Borges persistance layer.
+//
+// It should have the following methods:
+//
+// - get(type, id[, rev])
+//   get an item of the specified type,
+//   possibly a specific rev of that item.
+//   If no rev is given, return the last rev.
+//
+// - revisions(type, id)
+//   returns a list of revisions for the item
+//
+// - insert(type, item)
+//   add a new item to the collection of items
+//   of type
+//
+// - update(type, id, item)
+//   updates the item
+//
+// - remove(type, id)
+//   removes the item
+//
+// - list(type)
+//   list all items of specific type
+//
+// and can optionally override these methods:
+// - revert(type, id, rev)
+//   revert an item to a previous revision
+//
+// - lastRev(type, id)
+//   returns the last made revision identifier
+//   of the item (not the item itself)
+
 import uuid from 'uuid';
 
 export default class Persistor {
@@ -5,14 +42,23 @@ export default class Persistor {
 
   }
 
+  // create a new id
   _id() {
     return uuid.v1();
   }
 
-  async _lastRev(type, id) {
+  // get the last revision of an item
+  async lastRev(type, id) {
     var revs = await this.revisions(type, id);
-    return Number(revs[revs.length - 1]);
+    return revs[revs.length - 1];
   }
+
+  // revert and item to a previous revision
+  async revert(type, id, rev) {
+    var old = await this.get(type, id, rev);
+    return this.update(type, id, old);
+  }
+
 };
 
 class NotFoundError extends Error {
