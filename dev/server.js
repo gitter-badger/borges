6117dfefacff5dba6671b1b42ee8b4f799acff44
Romeo Van Snick <romeovs@gmail.com>
2015-05-07 15:48:32 +0200

    redirect dev server to borges

--- dev/server.js
+++ dev/server.js
@@ -1,5 +1,6 @@
 import Borges from '../src/server';
 import koa    from 'koa';
+import router from 'koa-router';
 
 var library = Borges({
   // base: '/borges'  // this is the default location of borges ui
@@ -8,8 +9,16 @@ var library = Borges({
 // create server
 var srv = koa();
 
+srv.use(router(srv));
+
 // use the Borges backend ui
 srv.use(library.server);
+
+srv.get('/', function * (next) {
+  this.response.set('refresh', '1;/borges');
+  this.body = 'Nothing here, redirecting you to Borges...';
+  yield next;
+});
 
 // start server
 var port = process.env.PORT || 3000;
