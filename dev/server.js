b09af902b112f51e6f8629e5376c3e27a70d8bf3
Romeo Van Snick <romeovs@gmail.com>
2015-05-12 17:28:11 +0200

    remove new-cap rule

--- dev/server.js
+++ dev/server.js
@@ -2,18 +2,20 @@ import Borges from '../src/server';
 import koa    from 'koa';
 import router from 'koa-router';
 
-var library = Borges({
+const library = Borges({
   // base: '/borges'  // this is the default location of borges ui
 });
 
 // create server
-var srv = koa();
+const srv = koa();
 
+// install routers
 srv.use(router(srv));
 
 // use the Borges backend ui
 srv.use(library.server);
 
+// refresh to borges
 srv.get('/', function * (next) {
   this.response.set('refresh', '1;/borges');
   this.body = 'Nothing here, redirecting you to Borges...';
@@ -21,6 +23,6 @@ srv.get('/', function * (next) {
 });
 
 // start server
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 srv.listen(port);
 console.log(`listening on port ${port}...`);
